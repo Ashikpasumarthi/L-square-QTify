a668a468794b867138ac7aaefc649ab9ee8e19a4
Ashikpasumarthi <pasumarthiashik88874@gmail.com>
2025-07-27 19:31:48 +0530

    Songs Swiper

--- qtify/src/Components/HomePage/homePage.js
+++ qtify/src/Components/HomePage/homePage.js
@@ -10,29 +10,32 @@ import { fetchTopAlbums } from "../../Slices/topAlbums";
 export default function HomePage() {
 
   const dispatch = useDispatch();
-      const topAlbums = useSelector((state) => state.topAlbums.topAlbums);
-      console.log("Top Albums in Section", topAlbums);
-      useEffect(() => {
-          async function fetchAlbums() {
-              console.log("Fetching top albums...");
-  
-              const result = await dispatch(fetchTopAlbums());
-              console.log("Result of fetchTopAlbums:", result);
-  
-              if (fetchTopAlbums.fulfilled.match(result)) {
-                  console.log("Albums loaded:", result.payload);
-              } else {
-                  console.error("Error loading albums:", result.error.message);
-              }
-          }
-          fetchAlbums();
-      }, [dispatch]);
-  
+  const topAlbums = useSelector((state) => state.topAlbums.topAlbums);
+  console.log("Top Albums in Section", topAlbums);
+  useEffect(() => {
+    async function fetchAlbums() {
+      console.log("Fetching top albums...");
+
+      const result = await dispatch(fetchTopAlbums());
+      console.log("Result of fetchTopAlbums:", result);
+
+      if (fetchTopAlbums.fulfilled.match(result)) {
+        console.log("Albums loaded:", result.payload);
+      } else {
+        console.error("Error loading albums:", result.error.message);
+      }
+    }
+    fetchAlbums();
+  }, [dispatch]);
+
   return (
     <div>
       <Hero />
-      <Section topAlbums={topAlbums} />
+      <Section albums={ topAlbums } />
       <Tabs />
     </div>
   );
 }
+
+
+//https://github.com/madhubala140/L-square-QTify   -  needful resource  -  https://github.com/madhubala140/L-square-QTify/tree/main
